08e72174a15d75ce83d0334784a92f2b8427c173
Segev2101 <segevshinar@gmail.com>
2025-02-24 16:40:13 +0200

    first_pass starting

--- src/first_pass.c
+++ src/first_pass.c
@@ -1,57 +1,32 @@
 #include "first_pass.h"
 #include "hashmap.h"
 #include "line.h"
+#include "globals.h"
 
-int first_pass(char *file_path) {
-    FILE *file = fopen(file_path, "r");
-    if (!file) {
-        printf(stderr, "Error: Cannot open file %s\n", file_path);
-        return -1;
-    }
+void printerror(char *error) {
+	printf("%s\n", error);
+}
+
+int first_pass (char *src_path){
+    char *line, *new_path;
+    FILE *file;
+
+	file = fopen(path, "r");
+	if (file == NULL) {
+		free_hashmap(symbolTable);
+		free(path);
+		return FAIL_CODE;
+	}
+
+    init_hashmap(symbolTable, TABLE_SIZE);
 
     IC = 100;
     DC = 0;
-    char *line = NULL; 
-    int is_symbol = 0; 
 
-    while ((line = read_line(file)) != NULL) {
-        Line *parsed_line = split_line(line);
-        if (!parsed_line) {
-            free(line);
-            continue;
-        }
+    while (line = read_line(file) != NULL) {
+       Line split_line *parsed_line = split_line(line);
+        
+    }
+    
 
-        if (parsed_line->label) {
-            is_symbol = 1;
-        }
-
-        if (strcmp(parsed_line->command, ".data") == 0 || strcmp(parsed_line->command, ".string") == 0) {
-            if (is_symbol) {
-                if (findSymbol(parsed_line->label) != -1) {
-                    fprintf(stderr, "Error: Symbol %s already exists\n", parsed_line->label);
-                } else {
-                    insertSymbol(parsed_line->label, DC, DATA);
-                }
-            }
-
-            if (strcmp(parsed_line->command, ".data") == 0) {
-                char **data_arguments = split_line(parsed_line->arguments[0]);
-                for (int i = 0; data_arguments[i] != NULL; i++) {
-                    /* Convert data_arguments[i] to integer and store it in memory */
-                    DC++;
-                }
-                /* Free the data arguments array */ 
-            } else if (strcmp(parsed_line->command, ".string") == 0) {
-                char *str = parsed_line->arguments[0];
-                for (int i = 0; str[i] != '\0'; i++) {
-                    /* Store each character in memory */
-                    DC++;
-                }
-                DC++;
-            }
-
-            free_line(parsed_line);
-            free(line);
-            continue;
-        }    
-/* I need to continue 8-20 */
+}
